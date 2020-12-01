74ddd6dfcb61003f7310d399ed616e274ac69654
Casper Sørensen <caspers@live.dk>
2020-12-01 17:08:21 +0100

    Update testing-deployment.sh

--- deployment/scripts/testing-deployment.sh
+++ deployment/scripts/testing-deployment.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 set -x
-ansible-playbook pull_webshop_front_end_testing.yml -i /etc/ansible/hosts --private-key /var/lib/jenkins/.ssh/id_rsa -u root
+ansible-playbook pull_webshop_front_end_testing.yml -i /etc/ansible/hosts --private-key /var/lib/jenkins/.ssh/id_rsa -u jenkins
 set +x
